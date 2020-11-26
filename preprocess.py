24574a162bc6219b3a63e3a03327626c857d3b16
Inhwan Lee <lih0905@lxper.com>
2020-11-26 05:54:10 +0000

    Handle only multiwords

--- preprocess.py
+++ preprocess.py
@@ -9,34 +9,50 @@
 
 import os
 import json
+import argparse
 import pickle
 import xml.etree.ElementTree as elemTree
 
 import pandas as pd
 
-data_path = 'Data'
-dict_path = 'Dict'
+# Argparse Setting
+parser = argparse.ArgumentParser(description='전처리 파라미터 설정')
+
+#training arguments
+parser.add_argument('--data', action='store_true')
+parser.add_argument('--data-path', type=str, default='./Data')
+parser.add_argument('--dict', action='store_true')
+parser.add_argument('--dict-path', type=str, default='./Dict')
+
 fname1 = 'NXLS1902008050.json'
 fname2 = 'SXLS1902008030.json'
 train_fname = 'processed_train.pickle'
 eval_fname = 'processed_eval.pickle'
 dict_fname = 'processed_dictionary.pickle'
 
-def process(sent_input):
+def process(sent_input, multiwords_set):
     """말뭉치 데이터의 문장이 주어졌을 때 다음 형식으로 변환
     [(단어1, 품사1, 의미순번1), (단어2, 품사2, 의미순번2), ... ]
     """
     result = []
-    for word_d in sent_input['morpheme']:
-        # {'id': 1, 'form': '2012', 'label': 'SN', 'word_id': 1, 'position': 1},
-        for wsd_d in sent_input['WSD']:
-            # {'word': '년', 'sense_id': 2, 'pos': 'NNB', 'begin': 4, 'end': 5, 'word_id': 1}
-            if word_d['form'] == wsd_d['word'] and word_d['word_id'] == wsd_d['word_id'] and word_d['label'] == wsd_d['pos']:
-                # 이 경우 (단어, 품사, 의미순번) 
-                result.append((word_d['form'], word_d['label'], wsd_d['sense_id']))
-                break
-        else: # 다의어가 아닌 경우는 의미순번을 제외
-            result.append((word_d['form'], word_d['label'], -1))
+    try:
+        for word_d in sent_input['morpheme']:
+            # {'id': 1, 'form': '2012', 'label': 'SN', 'word_id': 1, 'position': 1},
+            if word_d['form'] in multiwords_set:
+                for wsd_d in sent_input['WSD']:
+                    # {'word': '년', 'sense_id': 2, 'pos': 'NNB', 'begin': 4, 'end': 5, 'word_id': 1}
+                    if word_d['form'] == wsd_d['word'] and word_d['word_id'] == wsd_d['word_id'] and word_d['label'] == wsd_d['pos']:
+                        # 이 경우 (단어, 품사, 의미순번) 
+                        result.append((word_d['form'], word_d['label'], wsd_d['sense_id']))
+                        break
+                else: # 우리말샘 기준 다의어이긴 하나 WSD 데이터에 없는 경우는 제외
+                    result.append((word_d['form'], word_d['label'], -1))
+            else: # 다의어가 아닌 경우는 의미순번을 제외
+                result.append((word_d['form'], word_d['label'], -1))
+    except:
+        # 문장이 비어있는 경우
+        pass
+        
     return result
 
 def dict_to_data(dirname):
@@ -77,56 +93,69 @@ def dict_to_data(dirname):
 
 if __name__ == "__main__":
 
-    print("말뭉치 데이터 로딩...")
-    with open(os.path.join(data_path, fname1), "r") as json_file:
-        f1 = json.load(json_file)
+    args = parser.parse_args()
+    
+    data_path = args.data_path
+    dict_path = args.dict_path
 
-    with open(os.path.join(data_path, fname2), "r") as json_file:
-        f2 = json.load(json_file)
+    
+    if args.dict:
+        print("우리말샘 사전 데이터 로딩...")
+        # 우리말샘 사전 데이터 불러온 후 CSV 파일로 저장
+        dict_data = dict_to_data(dict_path)
 
-    # 데이터의 key는 'id', 'metadata', 'document' 이며, 이중 'document'가 필요한 데이터 부분
-    # 'document'는 기사들의 list로 이루어져 있으며, item은 딕셔너리이다.
-    # 이 딕셔너리 또한 'sentence'라는 key를 조회하면 실제 데이터를 얻을 수 있다.
-    data = f1['document'] + f2['document']
-    print(f"문맥 데이터는 {len(data)}개입니다.")
+        print(f'사전 데이터는 총 {len(dict_data)}개 입니다.')
 
-    print("말뭉치 데이터 변환 시작...")
-    train_results = []
-    eval_results = []
-    # 문맥 데이터 중 10번째마다 eval_data로 저장,
-    # 그외는 train_data로 저장
-    for i, paragraph in enumerate(data):
-        para_result = []
-        for sentence in paragraph['sentence']:
-            # 일부 오류나는 라인이 있어 강제로 패스
-            try:
-                para_result.append(process(sentence))
-            except:
-                break
-        # 오류가 나지 않은 문맥만 추가
-        else:
-            if i % 10 != 0:
-                train_results += para_result
-            elif i % 10 == 0:
-                eval_results += para_result
+        print("우리말샘 사전 데이터 저장...")
+        # 변환 파일 저장
+        with open(os.path.join(dict_path, dict_fname), 'wb') as f:
+            pickle.dump(dict_data, f)
+    
+    with open(os.path.join(dict_path, dict_fname), 'rb') as f:
+            urimal_dict = pickle.load(f)
+    multiwords_set = set(k for k, v in urimal_dict.items() if len(v['definition'])>1)
+    print(f"우리말샘 사전 기준 다의어는 {len(multiwords_set)}개입니다.")
+    
+    if args.data:
+        print("말뭉치 데이터 로딩...")
+        with open(os.path.join(data_path, fname1), "r") as json_file:
+            f1 = json.load(json_file)
 
-    print("변환된 말뭉치 데이터 파일로 저장...")
-    # 완료된 데이터를 pickle 데이터로 저장
-    with open(os.path.join(data_path, train_fname), "wb") as train_f:
-        pickle.dump(train_results, train_f)
-    with open(os.path.join(data_path, eval_fname), "wb") as eval_f:
-        pickle.dump(eval_results, eval_f)            
+        with open(os.path.join(data_path, fname2), "r") as json_file:
+            f2 = json.load(json_file)
+
+        # 데이터의 key는 'id', 'metadata', 'document' 이며, 이중 'document'가 필요한 데이터 부분
+        # 'document'는 기사들의 list로 이루어져 있으며, item은 딕셔너리이다.
+        # 이 딕셔너리 또한 'sentence'라는 key를 조회하면 실제 데이터를 얻을 수 있다.
+        data = f1['document'] + f2['document']
+        print(f"문맥 데이터는 {len(data)}개입니다.")
+
+        print("말뭉치 데이터 변환 시작...")
+        train_results = []
+        eval_results = []
         
+        # 문맥 데이터 중 10번째마다 eval_data로 저장,
+        # 그외는 train_data로 저장
+        i = 0
+        for paragraph in data:
+            for sentence in paragraph['sentence']:
+                sent = process(sentence, multiwords_set)
+                # 다의어가 하나 이상인 문장만 남김
+                if len([w for w in sent if w[-1] != -1]) > 0:
+                    i += 1
+                    if i % 10 != 0:
+                        train_results.append(sent)
+                    elif i % 10 == 0:
+                        eval_results.append(sent)
+                else:
+                    continue
+
         
-    print("우리말샘 사전 데이터 로딩...")
-    # 우리말샘 사전 데이터 불러온 후 CSV 파일로 저장
-    dict_data = dict_to_data(dict_path)
-    
-    print(f'사전 데이터는 총 {len(dict_data)}개 입니다.')
-    
-    print("우리말샘 사전 데이터 저장...")
-    # 변환 파일 저장
-    with open(os.path.join(dict_path, dict_fname), 'wb') as f:
-        pickle.dump(dict_data, f)
-    
-    print("전처리가 끝났습니다.")
+        print("변환된 말뭉치 데이터 파일로 저장...")
+        # 완료된 데이터를 pickle 데이터로 저장
+        with open(os.path.join(data_path, train_fname), "wb") as train_f:
+            pickle.dump(train_results, train_f)
+        with open(os.path.join(data_path, eval_fname), "wb") as eval_f:
+            pickle.dump(eval_results, eval_f)             
+            
+        print("전처리가 끝났습니다.")
